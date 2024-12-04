c0f3a1f1d02bee33aa38abe76419558208e7e9dd
churuizramos <ruizramos04@outlook.com>
2024-12-04 12:15:01 -0500

    changed order of some functions and added new get_recipe/id route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import pymysql  # Use pymysql if preferred
 from db_config import db_host,db_user,db_password,db_database
@@ -57,36 +57,6 @@ def insert_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-@app.route('/get_cards',methods=['GET'])
-def get_cards():
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-
-        limit = int(request.args.get('limit', 10))
-        offset = int(request.args.get('offset', 0))
-
-        load_all_query = """
-            SELECT Recipes.id, Recipes.title, Users.username AS userId, TimeofMeal.name AS timetoeat, Type.name AS mealType, Seasons.name AS season, Recipes.prepTime, Recipes.cookTime
-            FROM Recipes
-            JOIN Users ON Recipes.userId = Users.id
-            JOIN TimeofMeal ON Recipes.timetoeat = TimeofMeal.id
-            JOIN Type ON Recipes.mealType = Type.id
-            JOIN Seasons ON Recipes.season = Seasons.id
-            LIMIT %s OFFSET %s;
-        """
-
-        cursor.execute(load_all_query, (limit, offset))
-        result = cursor.fetchall()
-        cards = [{"id": row[0], "title":row[1], "userId":row[2], "timetoeat":row[3], "mealType":row[4], "season":row[5], "prepTime": row[6], "cookTime":row[7]} for row in result]
-
-        return jsonify(cards)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    finally:
-        cursor.close()
-        connection.close()
-
 @app.route('/get_cards_search', methods=['GET'])
 def get_cards_search():
     try:
@@ -147,6 +117,67 @@ def insert_user():
         return jsonify({"message":"User inserted successfully!"}), 201
     except Exception as e:
         return jsonify({"error": str(e)}),500
+
+@app.route('/get_cards',methods=['GET'])
+def get_cards():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        limit = int(request.args.get('limit', 10))
+        offset = int(request.args.get('offset', 0))
+
+        load_all_query = """
+            SELECT Recipes.id, Recipes.title, Users.username AS userId, TimeofMeal.name AS timetoeat, Type.name AS mealType, Seasons.name AS season, Recipes.prepTime, Recipes.cookTime
+            FROM Recipes
+            JOIN Users ON Recipes.userId = Users.id
+            JOIN TimeofMeal ON Recipes.timetoeat = TimeofMeal.id
+            JOIN Type ON Recipes.mealType = Type.id
+            JOIN Seasons ON Recipes.season = Seasons.id
+            LIMIT %s OFFSET %s;
+        """
+
+        cursor.execute(load_all_query, (limit, offset))
+        result = cursor.fetchall()
+        cards = [{"id": row[0], "title":row[1], "userId":row[2], "timetoeat":row[3], "mealType":row[4], "season":row[5], "prepTime": row[6], "cookTime":row[7]} for row in result]
+
+        return jsonify(cards)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/get_recipe/<int:recipe_id>', methods=['GET'])
+def get_recipe(recipe_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        query = """
+            SELECT Recipes.id, Recipes.title, Users.username AS userId, Recipes.created, TimeofMeal.name AS timetoeat, Type.name AS mealType, Seasons.name AS season, Recipes.prepTime, Recipes.cookTime, Recipes.description, Recipes.yield, Countries.name AS countryOfOrigin
+            FROM Recipes
+            JOIN Users ON Recipes.userId = Users.id
+            JOIN TimeofMeal ON Recipes.timetoeat = TimeofMeal.id
+            JOIN Type ON Recipes.mealType = Type.id
+            JOIN Seasons ON Recipes.season = Seasons.id
+            JOIN Countries ON Recipes.countryOfOrigin = Countries.id
+            WHERE Recipes.id = %s;
+        """
+
+        cursor.execute(query, recipe_id)
+        result = cursor.fetchall()
+
+        if not result:
+            abort(404, description="Recipe not found")
+
+        recipe = [{"id": row[0], "title":row[1], "userId":row[2], "created":row[3], "timetoeat":row[4], "mealType":row[5], "season":row[6], "prepTime": row[7], "cookTime":row[8], "description":row[9], "recyield":row[10], "countryOfOrigin":row[11]} for row in result]
+        return jsonify(recipe)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
