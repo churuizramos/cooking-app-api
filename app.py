b973bb7f6382a8e50ef245855b3928e644dcf804
churuizramos <ruizramos04@outlook.com>
2024-12-04 23:29:56 -0500

    changed the port value

--- app.py
+++ app.py
@@ -254,4 +254,5 @@ def get_cookbook(cookbook_id):
         connection.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
